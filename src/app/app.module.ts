0dad597fa3407c9565ecbf60fa04b35887be6e42
Arsenios Chatziantoniou <chatziantoniou.a@sga.gr>
2024-02-15 11:38:59 +0200

    PutNavComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
